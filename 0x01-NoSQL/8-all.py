2e3603d8ee2dd843f51ccbf117c3313dcb677266
Nkiru Ezefosie <nkiru.happiness@gmail.com>
2024-04-30 13:58:40 -0700

    NoSQL

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''List all documents in Python.
+'''
+
+
+def list_all(mongo_collection):
+    '''List all documents in Python.
+    '''
+    return [doc for doc in mongo_collection.find()]
